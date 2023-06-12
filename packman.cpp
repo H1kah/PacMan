6d5105ed9d70c8d430268bc2dc3d4958c913467f
H1karu <124624556+H1kah@users.noreply.github.com>
2023-06-12 20:21:05 -0300

    Update packman.cpp

--- packman.cpp
+++ packman.cpp
@@ -5,32 +5,36 @@ class PacMan
 {
 {
 public:
-//o public é para que as variáveis daqui possam ser mencionadas em outros lugares, sem problema algum.
+//o public é para que as variáveis daqui possam ser mencionadas em outros lugares, sem problema algum. No caso, aqui está o do pacman
     std::string cor;
     int boca;
     int olhos;
+//aqui estão as tais variáveis, as características que o Pac Man possui.
 };
 
 class Fantasminha
+//Para citar cada característica do personagem é preciso criar uma base, sendo o que poderá ser citada depois ou associada com algo
 {
 public:
+//o public é para que as variáveis daqui possam ser mencionadas em outros lugares, sem problema algum. No caso, aqui está o do fantasminha.
     std::string cor;
     int boca;
     int olhos;
 };
 
 int main()
+//parte principal do código, aqui qeu a mágica acontecerá
 {
     PacMan pacman;
     pacman.cor = "amarelo";
     pacman.boca = 1;
     pacman.olhos = 2;
-
+//apenas um personagem, não há outras versões dele. Aqui está as características dele, mostrando cada coisa que ele tem.
     Fantasminha fantasma_vermelho;
     fantasma_vermelho.cor = "vermelho";
     fantasma_vermelho.boca = 1;
     fantasma_vermelho.olhos = 2;
-
+//Existe vários fantasmas, as características são iguais mesmo, a diferença é que mostrará qual é qual é a cor. Ou seja, a base será a mesma, mas a cor mudará.
     Fantasminha fantasma_azul;
     fantasma_azul.cor = "azul";
     fantasma_azul.boca = 1;
